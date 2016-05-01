c59c700f4e98ffc4f2dda0f603050cc497139007
Irene Robb <irenerobb72@gmail.com>
2016-05-01 18:56:48 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: [
+    'webpack-dev-server/client?http://localhost:8080',
+    './dist/index.html'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    },
+    {
+      test: /\.scss$/,
+      loader: 'style!css!sass'
+    }
+  ]},
+  resolve: {
+    extensions: ['', '.js']
+  },
+  output: {
+    path: __dirname + '/',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './'
+  },
+  sassLoader: {
+     includePaths: [path.resolve(__dirname, "./styles")]
+   }
+}
---
+++ webpack.production.config.js
@@ -0,0 +1,33 @@
+const webpack = require('webpack')
+const path = require('path')
+
+module.exports = {
+  entry: [
+    './dist/index.html'
+  ],
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'envify-loader!babel'
+    },
+    {
+      test: /\.scss$/,
+      loader: 'style!css!sass'
+    }
+  ]},
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/',
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: './'
+  },
+  sassLoader: {
+     includePaths: [path.resolve(__dirname, "./styles")]
+   }
+}
